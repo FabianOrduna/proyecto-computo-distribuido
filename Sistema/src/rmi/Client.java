d69b21cf94e35a7529ed36b13cd055bea5e8e3fa
unknown <mcc06@2021MCC06.inge.itam.mx>
2021-01-31 11:55:48 -0600

    RMI con Hello World!

---
+++ Sistema/src/rmi/Client.java
@@ -0,0 +1,24 @@
+package rmi;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+
+    private Client() {}
+
+    public static void main(String[] args) {
+
+        String host = (args.length < 1) ? "148.205.36.207" : args[0];
+        try {
+            System.setProperty("java.rmi.server.hostname", host);
+            Registry registry = LocateRegistry.getRegistry(1010);
+            Hello stub = (Hello) registry.lookup("Hello");
+            String response = stub.sayHello();
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Sistema/src/rmi/Server.java
@@ -0,0 +1,36 @@
+package rmi;
+        
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.time.LocalDateTime;
+        
+public class Server extends UnicastRemoteObject implements Hello {
+        
+    public Server() throws RemoteException{}
+
+    @Override
+    public String sayHello(){
+        System.out.println("Hora de peticion: "+LocalDateTime.now().toString());
+        return "Hello, world!";
+    }
+        
+    public static void main(String args[]) {
+        
+        try {
+            Server obj = new Server();
+            //Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            //Registry registry = LocateRegistry.getRegistry();
+            Registry registry = LocateRegistry.createRegistry(1010);
+            registry.bind("Hello", obj);
+
+            System.out.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
