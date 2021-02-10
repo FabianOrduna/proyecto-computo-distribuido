52859fd1c606adfcaccf928a5dcb6d55bb3044c5
mcc14 <mcc14@2021MCC14.inge.itam.mx>
2021-02-10 12:56:14 -0600

    Metodos Mariana

--- Sistema/src/bd/ModeloVuelos.java
+++ Sistema/src/bd/ModeloVuelos.java
@@ -5,6 +5,7 @@
  */
 package bd;
 
+import java.rmi.RemoteException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -19,7 +20,7 @@ public class ModeloVuelos {
     private Connection c;
     
     public ModeloVuelos() throws SQLException{
-        this.c = ConnectionPool.getInstance().getConnection();
+        //this.c = ConnectionPool.getInstance().getConnection();
     }
     
     public int insertaViajero(String nombre) throws SQLException{
@@ -97,6 +98,99 @@ public class ModeloVuelos {
                 resultados.add(v);    
             }
         }catch(SQLException e){
+            System.out.println(e.toString());
+            return resultados;
+        }
+        return resultados;
+    }
+    
+    // Metodo que devuelve el historial de todos los vuelos
+    // No recibe parametros
+    // Devuelve un ArrayList de Vuelos
+    public ArrayList<Vuelo> vuelosHistoricos(){
+        ArrayList<Vuelo> resultados = new ArrayList();
+        String sql = "SELECT * FROM vuelo";
+        ResultSet resultado;
+        Vuelo v;
+        try{
+            PreparedStatement statement = c.prepareStatement(sql);
+            resultado = statement.executeQuery();
+            System.out.println(resultado.toString());
+            while(resultado.next()){
+                int resIdVuelo = Integer.parseInt(resultado.getString("id_vuelo"));
+                int resIdOrigen = Integer.parseInt(resultado.getString("id_origen"));
+                int resIdDestino = Integer.parseInt(resultado.getString("id_destino"));
+                String resFecha = resultado.getString("fecha");
+                v= new Vuelo(resIdVuelo, resIdOrigen,resIdDestino, resFecha);
+                resultados.add(v);    
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+            return resultados;
+        }
+        return resultados;    
+    }
+    
+    // Metodo que devuelve una tabla con las personas 
+    // que volaran en un vuelo especifico
+    // Recibe el identificador del vuelo
+    // Devuelve un ArrayList de Personas
+    public ArrayList<Persona> obtenerPersonasVuelo(int idVuelo){
+        ArrayList<Persona> resultados = new ArrayList();
+        String sql = "SELECT * FROM persona WHERE persona.id_persona in "
+                + "(SELECT persona_vuelo.id_persona FROM persona_vuelo WHERE persona_vuelo.id_vuelo = ?)";
+        ResultSet resultado;
+        Persona p;
+        try{
+            PreparedStatement statement = c.prepareStatement(sql);
+            statement.setInt(1,idVuelo);
+            resultado = statement.executeQuery();
+            System.out.println(resultado.toString());
+            while(resultado.next()){
+                int resIdPersona = Integer.parseInt(resultado.getString("id_persona"));
+                String resNombrePersona = resultado.getString("nombre");
+                p= new Persona(resIdPersona, resNombrePersona);
+                resultados.add(p);    
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+            return resultados;
+        }
+        return resultados;
+    }
+    
+    // Metodo que devuelve los vuelos que ocurrieron antes de una fecha especifica
+    // en el historial de una persona especifica
+    // Recibe la fecha en formato YYYY-MM-DD y el identificador de la Persona
+    // Devuelve un ArrayList de Vuelos
+    public ArrayList<Vuelo> vuelosAnterioresPersona(String fecha, int idPersona){
+        ArrayList<Vuelo> resultados = new ArrayList();
+        String sql = "SELECT DISTINCT persona.id_persona, persona.nombre, vuelo.id_vuelo, vuelo.fecha,"
+                + "(SELECT lugar.nombre FROM lugar WHERE lugar.id_lugar = vuelo.id_origen) as origen,"
+                + "(SELECT lugar.nombre FROM lugar WHERE lugar.id_lugar = vuelo.id_destino) as destino"
+                + "FROM persona INNER JOIN persona_vuelo ON persona_vuelo.id_persona = persona.id_persona"
+                + "INNER JOIN vuelo ON vuelo.id_vuelo = persona_vuelo.id_vuelo WHERE persona.id_persona = ?"
+                + "AND vuelo.fecha <= ?";
+        ResultSet resultado;
+        Vuelo v;
+        try{
+            PreparedStatement statement = c.prepareStatement(sql);
+            statement.setInt(1,idPersona);
+            statement.setString(2,fecha);
+            resultado = statement.executeQuery();
+            System.out.println(resultado.toString());
+            while(resultado.next()){
+                int resIdVuelo = Integer.parseInt(resultado.getString("id_vuelo"));
+                int resIdOrigen = Integer.parseInt(resultado.getString("id_origen"));
+                int resIdDestino = Integer.parseInt(resultado.getString("id_destino"));
+                String resFecha = resultado.getString("fecha");
+                v= new Vuelo(resIdVuelo, resIdOrigen,resIdDestino, resFecha);
+                resultados.add(v);     
+            }
+        }
+        catch(Exception e){
             System.out.println(e.toString());
             return resultados;
         }
