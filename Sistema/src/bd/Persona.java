f81f792e267f34eee28acd46653d33570c1ce192
unknown <mcc06@2021MCC06.inge.itam.mx>
2021-02-10 22:05:24 -0600

    Imprime persona

--- Sistema/src/bd/Persona.java
+++ Sistema/src/bd/Persona.java
@@ -36,6 +36,11 @@ public class Persona implements Serializable{
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + '}';
+    }
     
     
 }
