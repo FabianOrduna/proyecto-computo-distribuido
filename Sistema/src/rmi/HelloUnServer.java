741e8f8471563cc1a841fbd43ef4a34f8bb9a0e7
savrgg <savrgg@users.noreply.github.com>
2021-02-16 20:36:26 -0600

    renombrar archivos a unServer

---
+++ Sistema/src/rmi/HelloUnServer.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rmi;
+
+import bd.Lugar;
+import bd.Persona;
+import bd.Vuelo;
+import java.io.IOException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+
+/**
+ *
+ * @author mcc06
+ */
+public interface Hello extends Remote{
+    String sayHello(String persona) throws RemoteException;
+    String sayHello() throws RemoteException;
+    int insertaAlumno(String nombre, String paterno, String materno) throws RemoteException;
+    int actualizaAlumno(int idAlumno, String nombre, String paterno, String materno) throws RemoteException;
+      
+    public byte[] crearLlave(int llaveId) throws RemoteException; //M
+    public void coordLlave(byte[] bobPubKeyEnc) throws RemoteException, NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException;
+    
+    public byte[] enviarPrueba() throws RemoteException, IOException, IllegalBlockSizeException, BadPaddingException;
+    public byte[] obtenParametrosDeCifrado() throws IOException;
+    
+    public ArrayList<Vuelo> vuelosHistoricos() throws RemoteException; //M
+    public ArrayList<Vuelo> vuelosDisponibles(String fecha) throws RemoteException; //S
+    public Vuelo obtenerVuelo(int idVuelo) throws RemoteException; //F +
+    public ArrayList<Persona> obtenerPersonasVuelo(int idVuelo) throws RemoteException; //M
+    public ArrayList<Vuelo> vuelosHistoricosPersona(int idPersona) throws RemoteException; //S
+    public ArrayList<Vuelo> vuelosDisponiblesPersona(String fecha, int idPersona) throws RemoteException; //F +
+    public ArrayList<Vuelo> vuelosAnterioresPersona(String fecha, int idPersona) throws RemoteException; //M
+    public ArrayList<Lugar> obtenerLugares() throws RemoteException; //S
+    public ArrayList<Vuelo> vuelosOrigenDestino(int idOrigen, int idDestino) throws RemoteException; //F
+
+    
+    
+}
